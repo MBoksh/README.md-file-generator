e785478ce78e7d344c15fb2125c1aa808c943345
MBoksh <mikaaeelboksh@hotmail.com>
2023-01-05 21:47:52 +0000

    created functions to add answers to the generated readme file

--- index.js
+++ index.js
@@ -1,75 +1,88 @@
 // TODO: Include packages needed for this application
 // Added the required packages needed
-const inquirer = require('inquirer');
-const fs = require('fs');
-const path = require('path');
-const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 
 // Questions used to create the appropriate readme file by user
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Write a brief description of your project:',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'What were the steps/commands taken to create this project?',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'What can this project be used for?',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'What can this project be used for?',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'What license should your project have?',
-        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'NONE'],
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Who contributed to the creation of this project?',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Were there any tests for this project?',
-    },
-    {
-        type: 'input',
-        name: 'username',
-        message: 'What is your GitHub username?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?',
-    },
-
-
-
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Write a brief description of your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What were the steps/commands taken to create this project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What can this project be used for?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What can this project be used for?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What license should your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "NONE"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Who contributed to the creation of this project?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Were there any tests for this project?",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to create readme file
+function writeToFile(fileName, data) {
+  // Writes inputted data to readme file
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("Successfully created readme file")
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+// Function initialises inquirer to ask questions to create readme file
+function init() {
+  inquirer
+    .prompt(questions)
+    // Puts user input into generateMarkdown function to create readme
+    .then((data) => {
+      const readMeFileUserInput = generateMarkdown(data);
+      //Answers are written to the readme file created
+      writeToFile("README.md", readMeFileUserInput);
+    });
+}
 
 // Function call to initialize app
 init();
