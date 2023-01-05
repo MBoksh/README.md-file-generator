c33c5752bd5fb8cbdadf4860ade8b82e467e725e
MBoksh <mikaaeelboksh@hotmail.com>
2023-01-05 18:17:44 +0000

    added required packages

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // TODO: Include packages needed for this application
+// Added the required packages needed
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
+
+// Questions used to create the appropriate readme file by user
 const questions = [];
 
 // TODO: Create a function to write README file
