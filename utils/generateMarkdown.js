70ea1dcd465e67fd978a20f7991bd14cfc8f08ca
MBoksh <mikaaeelboksh@hotmail.com>
2023-01-06 19:24:40 +0000

    created the function that generates the markdown for file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,35 +2,81 @@
 // If there is no license, return an empty string
 // Function creates the chosen license badge and renders it to the readme file
 function renderLicenseBadge(license) {
-  if (license !== 'NONE') {
+  if (license !== "NONE") {
     return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
   }
-  return '';
+  return "";
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 // Function that renders the link of the license
 function renderLicenseLink(license) {
-  if (license !== 'NONE') {
+  if (license !== "NONE") {
     return `\n* [License](#license)\n`;
   }
-  return '';
+  return "";
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
+// Function that displays the chosen license under the license section
 function renderLicenseSection(license) {
-  if (license !== 'NONE') {
+  if (license !== "NONE") {
     return `## License
-    This license for this project is: ${license}`;
-    }
-    return '';
+    The license for this project is: ${license}`;
+  }
+  return "";
 }
 
 // TODO: Create a function to generate markdown for README
+// Function that generates the markdown for readme file
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+ 
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ${renderLicenseSection(data.license)}
+
+  The license link is ${renderLicenseLink(data.license)}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  Any questions can be asked by contacting the GitHub user: ${data.username} 
+  at the following email ${data.email}
+
+
+
+
 
 `;
 }
